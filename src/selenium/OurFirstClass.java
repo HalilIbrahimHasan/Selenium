0c854ad85e61285918c70d16db28a2d15f8683e8
Ibrahim Settar <hisetkal@gmail.com>
2020-03-14 01:13:22 -0400

    next cooment

--- src/selenium/OurFirstClass.java
+++ src/selenium/OurFirstClass.java
@@ -4,6 +4,12 @@ public class OurFirstClass {
 	
 	
 	public static void main(String[] args) {
-		System.out.println("Settar ");
+		System.out.println("Github class is ready!");
+		
+		int n2 =1;
+		//Now I have more improvements
+		for(int n1= 1 ; n2<=10; n2++)
+		System.out.println(n1 + " X " + n2 + " = " + n1 * n2);
+		
 	}
 }
